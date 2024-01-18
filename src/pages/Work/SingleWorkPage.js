7834d496d71ad5fcbc25044be02f429ec193f44b
RohanCheera <cheerarohan@gmail.com>
2024-01-18 08:58:30 +0530

    timeline ,woork changes

---
+++ src/pages/Work/SingleWorkPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './singleworkpage.css'
+
+function SingleWorkPage() {
+  return (
+    <div className='singleworkpage'></div>
+  )
+}
+
+export default SingleWorkPage
--- src/pages/Work/Work.js
+++ src/pages/Work/Work.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import { Card } from 'react-bootstrap'
 import './work.css'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 
 function Work() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Animation duration in milliseconds
+      offset: 100, // Offset (in px) from the top of the viewport to trigger animations
+      easing: 'ease-in-out', // Animation easing
+      delay: 200, // Delay (in ms) before animations start
+    });
+
+    // Optionally clean up AOS on component unmount
+    return () => AOS.refresh();
+  }, []);
   return (
     <div className='work'>
+      {/* <div data-aos="fade-right">
       <Card className='work-card'>
         <Card.Img variant="top" src="https://images.pexels.com/photos/733853/pexels-photo-733853.jpeg?auto=compress&cs=tinysrgb&w=600" />
-        <Card.Body>
+        <Card.Body className='work-card-content'>
           <Card.Title>Card Title</Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the
@@ -15,9 +29,11 @@ function Work() {
           </Card.Text>
         </Card.Body>
       </Card>
+      </div>
+      <div data-aos="fade-left">
       <Card className='work-card'>
         <Card.Img variant="top" src="https://images.pexels.com/photos/733853/pexels-photo-733853.jpeg?auto=compress&cs=tinysrgb&w=600" />
-        <Card.Body>
+        <Card.Body className='work-card-content'>
           <Card.Title>Card Title</Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the
@@ -25,108 +41,78 @@ function Work() {
           </Card.Text>
         </Card.Body>
       </Card>
-      <Card className='work-card'>
-        <Card.Img variant="top" src="https://images.pexels.com/photos/733853/pexels-photo-733853.jpeg?auto=compress&cs=tinysrgb&w=600" />
-        <Card.Body>
+      </div> */}
+         <Card className='work-card' data-aos='fade-right'>
+            <Card.Img className='workcard-content-left' variant="top" src='https://images.pexels.com/photos/16124552/pexels-photo-16124552/free-photo-of-man-walking-along-running-subway-tray.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' />
+            <Card.Body className='work-card-content'>
           <Card.Title>Card Title</Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </Card.Text>
         </Card.Body>
-      </Card>
-      <Card className='work-card'>
-        <Card.Img variant="top" src="https://images.pexels.com/photos/733853/pexels-photo-733853.jpeg?auto=compress&cs=tinysrgb&w=600" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className='work-card'>
-        <Card.Img variant="top" src="https://images.pexels.com/photos/733853/pexels-photo-733853.jpeg?auto=compress&cs=tinysrgb&w=600" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className='work-card'>
-        <Card.Img variant="top" src="https://images.pexels.com/photos/733853/pexels-photo-733853.jpeg?auto=compress&cs=tinysrgb&w=600" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className='work-card'>
-        <Card.Img variant="top" src="https://images.pexels.com/photos/733853/pexels-photo-733853.jpeg?auto=compress&cs=tinysrgb&w=600" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className='work-card'>
-        <Card.Img variant="top" src="https://images.pexels.com/photos/733853/pexels-photo-733853.jpeg?auto=compress&cs=tinysrgb&w=600" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className='work-card'>
-        <Card.Img variant="top" src="https://images.pexels.com/photos/733853/pexels-photo-733853.jpeg?auto=compress&cs=tinysrgb&w=600" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className='work-card'>
-        <Card.Img variant="top" src="https://images.pexels.com/photos/733853/pexels-photo-733853.jpeg?auto=compress&cs=tinysrgb&w=600" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className='work-card'>
-        <Card.Img variant="top" src="https://images.pexels.com/photos/733853/pexels-photo-733853.jpeg?auto=compress&cs=tinysrgb&w=600" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className='work-card'>
-        <Card.Img variant="top" src="https://images.pexels.com/photos/733853/pexels-photo-733853.jpeg?auto=compress&cs=tinysrgb&w=600" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
+          </Card>
 
-    </div>
+
+          <Card className='work-card' data-aos='fade-left'>
+            <Card.Img className='workcard-content-left' variant="top" src="https://images.pexels.com/photos/1921326/pexels-photo-1921326.jpeg?auto=compress&cs=tinysrgb&w=600" />
+            <Card.Body className='work-card-content'>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+          </Card>
+
+          <Card className='work-card' data-aos='fade-right'>
+            <Card.Img className='workcard-content-left' variant="top" src="https://images.pexels.com/photos/8627841/pexels-photo-8627841.jpeg?auto=compress&cs=tinysrgb&w=600" />
+            <Card.Body className='work-card-content'>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+          </Card>
+
+          <Card className='work-card' data-aos='fade-left'>
+            <Card.Img className='workcard-content-left' variant="top" src="https://images.pexels.com/photos/907489/pexels-photo-907489.jpeg?auto=compress&cs=tinysrgb&w=600" />
+            <Card.Body className='work-card-content'>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+          </Card>
+
+          <Card className='work-card' data-aos='fade-right'>
+            <Card.Img className='workcard-content-left' variant="top" src="https://images.pexels.com/photos/669615/pexels-photo-669615.jpeg?auto=compress&cs=tinysrgb&w=600" />
+            <Card.Body className='work-card-content'>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+          </Card>
+
+          <Card className='work-card' data-aos='fade-left'>
+            <Card.Img className='workcard-content-left' variant="top" src="https://images.pexels.com/photos/3772711/pexels-photo-3772711.jpeg?auto=compress&cs=tinysrgb&w=600" />
+            <Card.Body className='work-card-content'>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+          </Card>
+      </div>
+      
+      
+
+
   )
 }
 
